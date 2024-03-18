8ae65edf9aebac93261cc95b0ecd9787f5ea7897
Tony Spataro <github.com@tracker.xeger.net>
2024-03-17 20:01:05 -0700

    Keep hacking on the org bar

--- packages/app/studio/src/routes/experiments/sdk.lazy.tsx
+++ packages/app/studio/src/routes/experiments/sdk.lazy.tsx
@@ -1,8 +1,5 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@crossnokaye/ui-primitives/card';
-import { Skeleton } from '@crossnokaye/ui-primitives/skeleton';
 import { useListUserOrgs_facilitiesExtended } from '@scratch/svc.atlas/hooks/iam';
 import { Organization_FacilitiesExtended } from '@scratch/svc.atlas/models/views';
-import { Notice } from '@scratch/ui.elements/notice';
 import { OrgBar } from '@scratch/ui.elements/org-bar';
 import { createLazyFileRoute } from '@tanstack/react-router';
 import { FacilityScopedLayout } from '../../layouts/facility-scoped';
@@ -22,32 +19,7 @@ function UserOrgList() {
         <div>
           <OrgBar orgs={data?.length ? data : []} />
         </div>
-        <div>
-          <Card>
-            <CardHeader>
-              <CardTitle>My Organizations</CardTitle>
-            </CardHeader>
-            <CardContent>
-              {error ? (
-                <Notice text={error.toString()} variant="destructive" />
-              ) : isPending ? (
-                <div className="space-y-2">
-                  <Skeleton className="h-4 w-[250px]" />
-                  <Skeleton className="h-4 w-[200px]" />
-                  <Skeleton className="h-4 w-[300px]" />
-                </div>
-              ) : (
-                <div className="space-y-2">
-                  {data.map((org) => (
-                    <div key={org.organizationId}>
-                      {org.displayName} ({countFac(org)} facilities)
-                    </div>
-                  ))}
-                </div>
-              )}
-            </CardContent>
-          </Card>
-        </div>
+        <div>here is some stuff about a facility</div>
       </div>
     </FacilityScopedLayout>
   );
--- packages/ui/elements/src/org-bar.tsx
+++ packages/ui/elements/src/org-bar.tsx
@@ -1,8 +1,34 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@crossnokaye/ui-primitives/accordion';
 import { Avatar, AvatarFallback } from '@crossnokaye/ui-primitives/avatar';
+import { Separator } from '@crossnokaye/ui-primitives/separator';
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@crossnokaye/ui-primitives/sheet';
 import { Skeleton } from '@crossnokaye/ui-primitives/skeleton';
+import { Link } from '@tanstack/react-router';
+
+interface FacilitySubset {
+  facilityId: string;
+  displayName: string;
+}
+
+interface OrganizationSubset {
+  organizationId: string;
+  displayName: string;
+  facilities: FacilitySubset[];
+}
 
 export interface OrgBarProps {
-  orgs?: { organizationId: string; displayName: string }[];
+  orgs?: OrganizationSubset[];
 }
 
 function shorten(displayName: string) {
@@ -13,19 +39,27 @@ function shorten(displayName: string) {
   return fallback;
 }
 
-export function OrgBar({ orgs: orgs }: OrgBarProps) {
+function representOrg(org: OrganizationSubset) {
   return (
-    <div className="flex flex-col sticky left-0 h-full z-50">
-      <Avatar>
-        <AvatarFallback>CK</AvatarFallback>
-      </Avatar>
-      <hr />
+    <Avatar key={org.organizationId}>
+      <AvatarFallback>{shorten(org.displayName)}</AvatarFallback>
+    </Avatar>
+  );
+}
+
+function representFacility(fac: FacilitySubset) {
+  return (
+    <Avatar key={fac.facilityId}>
+      <AvatarFallback>{shorten(fac.displayName)}</AvatarFallback>
+    </Avatar>
+  );
+}
+
+function Teaser({ orgs }: Pick<OrgBarProps, 'orgs'>) {
+  return (
+    <div className="grid grid-cols-1 sticky left-0 h-full z-50 m-1">
       {orgs ? (
-        orgs.map((org) => (
-          <Avatar key={org.organizationId}>
-            <AvatarFallback>{shorten(org.displayName)}</AvatarFallback>
-          </Avatar>
-        ))
+        orgs.map(representOrg)
       ) : (
         <>
           <Skeleton className="rounded-full" />
@@ -33,8 +67,52 @@ export function OrgBar({ orgs: orgs }: OrgBarProps) {
           <Skeleton className="rounded-full" />
         </>
       )}
-      <hr />
+      <Separator className="my-1" />
       (sess)
     </div>
+  );
+}
+
+function Picker({ orgs }: Pick<OrgBarProps, 'orgs'>) {
+  return (
+    <Accordion type="multiple" className="w-full">
+      {orgs?.map((org) => (
+        <AccordionItem value={org.organizationId}>
+          <AccordionTrigger>
+            <div className="flex flex-row justify-items-start items-center">
+              {representOrg(org)}&nbsp;{org.displayName}
+            </div>
+          </AccordionTrigger>
+          <AccordionContent className="grid grid-cols-1">
+            {org.facilities.map((facility) => (
+              // TODO: figure out routing scheme for facility display
+              <Link
+                to={`#${facility.facilityId}`}
+                className="flex flex-row justify-items-start items-center"
+              >
+                {representFacility(facility)}&nbsp;{facility.displayName}
+              </Link>
+            ))}
+          </AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+}
+
+export function OrgBar({ orgs }: OrgBarProps) {
+  return (
+    <Sheet>
+      <SheetTrigger disabled={!orgs?.length}>
+        <Teaser orgs={orgs} />
+      </SheetTrigger>
+      <SheetContent side="left" className="overflow-y-auto">
+        <SheetHeader>
+          <SheetTitle>(logo)&nbsp;ATLAS&nbsp;Platform</SheetTitle>
+          <Separator />
+        </SheetHeader>
+        <Picker orgs={orgs} />
+      </SheetContent>
+    </Sheet>
   );
 }
